782ed6573bced3b27896b42c4edd3fa69e325399
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-06-06 21:25:04 +0300

    feat: handlerError.ts

--- src/utils/errorResponse.ts
+++
@@ -1,5 +0,0 @@
-import { getReasonPhrase } from 'http-status-codes';
-import { Response } from 'express';
-
-export const errorResponse = (res: Response, code: number): Response =>
-  res.status(code).json({ message: getReasonPhrase(code) });

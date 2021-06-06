fec0f82e397c66cb86aeecce3dbfbe3734c5c72c
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-06-06 21:37:38 +0300

    refactor: correct config.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,6 +5,13 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-export const { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY } =
+const { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE } =
   process.env;
-export const AUTH_MODE = process.env['AUTH_MODE'] === 'true';
+
+export const env = {
+  PORT,
+  NODE_ENV,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+  AUTH_MODE: AUTH_MODE === 'true',
+};
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import { PORT } from './common/config';
+import { env } from './common/config';
 import app from './app';
 
-app.listen(PORT, () =>
-  process.stdout.write(`App is running on http://localhost:${PORT}\n`)
+app.listen(env.PORT, () =>
+  process.stdout.write(`App is running on http://localhost:${env.PORT}\n`)
 );

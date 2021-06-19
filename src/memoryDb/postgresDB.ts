5ec946db2e4f2b37fb6d7fac7881eccc362aa71e
Alex Malagor <aleksandr_pisarik@epam.com>
2021-06-19 17:51:00 +0300

    refactor: connection to DB by Uporold

--- src/memoryDb/postgresDB.ts
+++ src/memoryDb/postgresDB.ts
@@ -1,36 +1,7 @@
-import { getConnection, createConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
 import { config } from '../common/ormconfig';
 
-const connectToDB = async () => {
-  let connection;
-
-  try {
-    // console.log('Check current connection to DB...');
-    connection = await getConnection();
-  } catch (e) {
-    process.stderr.write(`Result: No current connection!\n`);
-  }
-
-  try {
-    if (connection) {
-      if (!connection.isConnected) {
-        await connection.connect();
-      }
-    } else {
-      // console.log('Create new connection to DB...');
-      await createConnection(config);
-    }
-  } catch (e) {
-    process.stderr.write(e);
-  }
-  process.stdout.write('Successfully Connected\n');
-};
-
-export const tryDBConnection = async (cb: () => void): Promise<void> => {
-  try {
-    await connectToDB();
-    cb();
-  } catch (e) {
-    process.stderr.write('Error DB connection', e);
-  }
+export const connect = async (): Promise<void> => {
+  const connection = await createConnection(config);
+  await connection.runMigrations();
 };
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,14 @@
 import { env } from './common/config';
 import app from './app';
-import { tryDBConnection } from './memoryDb/postgresDB';
+import { connect } from './memoryDb/postgresDB';
 
-// db.sync()
-//   .then(() => {
-//     app.listen(env.PORT, () =>
-//       process.stdout.write(`App is running on http://localhost:${env.PORT}\n`)
-//     );
-//   })
-//   .catch((error: Error) => {
-//     process.stderr.write(`${error.name}`);
-//     process.exit(1);
-//   });
-
-tryDBConnection(() => {
-  app.listen(env.PORT, () =>
-    process.stdout.write(`App is running on http://localhost:${env.PORT}\n`)
-  );
-}).catch((error: Error) => {
-  process.stderr.write(`${error.name}`);
-  process.exit(1);
-});
+connect()
+  .then(() => {
+    app.listen(env.PORT, () =>
+      process.stdout.write(`App is running on http://localhost:${env.PORT}\n`)
+    );
+  })
+  .catch((error: Error) => {
+    process.stderr.write(`${error.name}`);
+    process.exit(1);
+  });

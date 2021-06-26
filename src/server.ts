4c69b06d9a2d155430aa11ed0097503e4bc0e587
Alex Malagor <aleksandr_pisarik@epam.com>
2021-06-26 13:43:07 +0300

    refactor: return connection to DB by ts config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
+import { createConnection } from 'typeorm';
 import { env } from './common/config';
 import app from './app';
-import { connect } from './memoryDb/postgresDB';
+import config from './common/ormconfig';
 
-connect()
+createConnection(config)
   .then(() => {
     app.listen(env.PORT, () =>
       process.stdout.write(`App is running on http://localhost:${env.PORT}\n`)
